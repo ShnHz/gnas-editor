d32b11ec4923e01294efb7146a5a9b5a459a634e
SangHangNing <664652740@qq.com>
2022-12-08 22:23:48 +0800

    pref:component editor

--- package/gnas-editor/components/common/Editor.vue
+++ package/gnas-editor/components/common/Editor.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="editor-wrap" v-if="(type == 'markdownit')">
-        <div ref="editorMain" class="editor-main-wrap" contentEditable="true" :style="{ height: height, width: width}" @keyup="handleChange"></div>
+        <button @click="selectionChange">test</button>
+        <div ref="editorMain" class="editor-main-wrap" contentEditable="true" :style="{ height: height, width: width }"
+            @keyup="handleChange"></div>
     </div>
 </template>
 <script>
@@ -24,17 +26,38 @@ export default {
     },
     data() {
         return {
-            data: `123 \n+ 1 \n+ 2`
+            data: `123 \n+ 1 \n+ 2`,
+            editor: null
         }
     },
-    mounted(){
-        this.$refs.editorMain.innerText = this.data
-        this.$emit('change',this.data)
+    mounted() {
+        this.editor = this.$refs.editorMain
+        this.editor.innerText = this.data
+        this.$emit('change', this.data)
     },
-    methods:{
-        handleChange(){
-            this.data = this.$refs.editorMain.innerText
-            this.$emit('change',this.data)
+    methods: {
+        handleChange() {
+            this.data = this.editor.innerText
+            this.$emit('change', this.data)
+        },
+        selectionChange() {
+            let selectionStr = getSelection()
+            let replaceSelection = this.getReplaceSelection(selectionStr)
+        },
+        // 获取选中值
+        getSelection() {
+            let selectionStr = null
+            if (window.getSelection) {
+                selectionStr = window.getSelection().toString()
+            } else if (document.selection) {
+                selectionStr = document.selection.toString()
+            }
+
+            return selectionStr
+        },
+        // 获取替换值
+        getReplaceSelection(selectionStr){
+            return selectionStr
         }
     }
 }
